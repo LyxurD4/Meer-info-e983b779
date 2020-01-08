aea0890ba35ff5c41348d691dd42d1abcb15d5b3
LyxurD4 <mdoes@roc-dev.com>
2020-01-08 10:58:04 +0100

    goed

--- exercise.sql
+++ exercise.sql
@@ -1,11 +1,11 @@
 create table planeten (
     naam varchar(30),
-    diameter varchar(30),
-    afstand_tot_de_zon varchar(30),
-    massa_tov_de_aarde varchar(30)
+    diameter int(30),
+    afstand_tot_de_zon int(30),
+    massa_tov_de_aarde int(30)
     );
 
-insert into planeten (Naam, Diameter, Afstand_tot_de_zon, Massa_tov_de_aarde) values 
+insert into planeten (naam, diameter, afstand_tot_de_zon, massa_tov_de_aarde) values 
 ('Zon', 1392000, null, 332946),
 ('Mercurius', 4880, 57910000, 0.1),
 ('Venus', 12104, 108208930, 0.9),
