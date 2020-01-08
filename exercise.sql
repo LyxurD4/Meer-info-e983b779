3104af4f5a7d9e55ec98e0e4865ac1913f48c650
LyxurD4 <mdoes@roc-dev.com>
2020-01-08 10:43:52 +0100

    verbetering

--- exercise.sql
+++ exercise.sql
@@ -1,13 +1,13 @@
-create table planeten (Naam varchar(30));
-alter table planeten
-add Diameter varchar(255);
-alter table planeten
-add Afstand_tot_de_zon varchar(255);
-alter table planeten
-add Massa_tov_de_aarde varchar(255);
-insert into planeten (Naam, Diameter, Afstand_tot_de_zon, Massa_tov_de_aarde)
-values ('Zon', 1392000, null, 332946);
-values ('Mercurius', 4880, 57910000, 0.1);
-values ('Venus', 12104, 108208930, 0.9);
-values ('Aarde', 12756, 149597870, 1);
-values ('Mars', 6794, 227936640, 0.1);
+create table planeten (
+    naam varchar(30),
+    diameter varchar(30),
+    afstand_tot_de_zon varchar(30),
+    massa_tov_de_aarde varchar(30)
+    );
+
+insert into planeten (Naam, Diameter, Afstand_tot_de_zon, Massa_tov_de_aarde) values 
+('Zon', 1392000, null, 332946),
+('Mercurius', 4880, 57910000, 0.1),
+('Venus', 12104, 108208930, 0.9),
+('Aarde', 12756, 149597870, 1),
+('Mars', 6794, 227936640, 0.1);
